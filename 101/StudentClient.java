2b3db4bbc2a37e1e6b934458d47958202d6bf0ca
ar406 <66774262+ar406@users.noreply.github.com>
2024-01-02 18:46:14 +0100

    Create StudentClient.java

---
+++ 101/StudentClient.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import student.Student;
+
+public class Client{
+//Student student = new Student();
+//student.main();
+//student.setAge(30);
+//student.setName("Mark");
+//student.setAddress("Washington Street, 10043, NY");
+//student.setEmail("mark@example.local");
+
+public static void main(String[] args) {
+System.out.println("This is the student client");
+Student student = new Student("Mark",30,"Washington Street, 10043, NY","mark@example.local");
+//student.main();
+student.setAge(30);
+student.setName("Mark");
+student.setAddress("Washington Street, 10043, NY");
+student.setEmail("mark@example.local");
+System.out.println("Returning values");
+
+int studentAge;
+String studentName;
+studentAge = student.getAge();
+studentName = student.getName();
+System.out.format("New student name is %s, age is %d",studentName,studentAge);
+}
+
+
+}
